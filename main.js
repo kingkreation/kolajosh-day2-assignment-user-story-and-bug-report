92a90ac8f991a2cb2269f0225fa9685baad34183
kingkreation <olujosh459@gmail.com>
2025-05-23 22:23:14 +0100

    Update Html, CSS and added Js

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-console.log('Hello World!');
+$(document).ready(function() {
+  $('.lightbox').click(function() {
+    $('#lightbox').css('display', 'block');
+    $('#lightbox-img').attr('src', $(this).attr('src'));
+  });
+  
+  $('.close').click(function() {
+    $('#lightbox').css('display', 'none');
+  });
+  
+  $(window).click(function(event) {
+    if (event.target.id === 'lightbox') {
+      $('#lightbox').css('display', 'none');
+    }
+  });
+});
